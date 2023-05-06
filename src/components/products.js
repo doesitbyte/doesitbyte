82ee13515d122cb5aa14f3893e6e488f5be6f104
Kaustubh <kaustubhsharma999@gmail.com>
2023-05-06 15:49:52 +0530

    Added title & favicon

--- src/components/products.js
+++ src/components/products.js
@@ -1,13 +1,13 @@
-import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import { ArrowRightIcon } from "@heroicons/react/24/solid";
 
 const Products = () => {
     return (
         <div id="products" className="pt-3 pb-7">
             <div className="flex justify-between w-full">
                 <p className="text-lg font-bold">Products</p>
-                <div className="flex">
-                    <p>All</p>
-                    <ArrowRightIcon role='img' title='Use system theme' className='h-4 w-4 dark:stroke-gray-400 dark:hover:stroke-gray-50 stroke-gray-600 hover:stroke-gray-950' />
+                <div className="flex group items-center justify-center cursor-pointer">
+                    <p className="group-hover:opacity-100 opacity-75 mr-1">All</p>
+                    <ArrowRightIcon role='img' title='Use system theme' className='h-4 w-4 stroke-3 group-hover:opacity-100 opacity-75 dark:stroke-gray-400 stroke-gray-600' />
                 </div>
             </div>
         </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,22 @@
-import Header from '@/components/header';
-import Hero from '@/components/hero';
-import Products from '@/components/products';
+import Head from "next/head";
+import Header from "@/components/header";
+import Hero from "@/components/hero";
+import Products from "@/components/products";
 
 const Home = () => {
   return (
-    <main className='w-full sm:max-w-xl pl-4 pr-4 m-auto'>
-      <Header />
-      <Hero />
-      <Products />
-    </main>
-  )
-}
+    <>
+      <Head>
+        <title>ks.dev</title>
+        <link rel="shortcut icon" href="/favicon.png" />
+      </Head>
+      <main className="w-full sm:max-w-xl pl-4 pr-4 m-auto">
+        <Header />
+        <Hero />
+        <Products />
+      </main>
+    </>
+  );
+};
 
 export default Home;
